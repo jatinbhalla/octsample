6b40911f982bde6c5d5c1d199dd8ee47f336fa2d
jatinbhalla <55462128+jatinbhalla@users.noreply.github.com>
2024-10-08 13:54:56 +0530

    terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "win-vm-iis"
+  description = "Prefix of the resource name"
+}
